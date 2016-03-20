fa7c0c001f7d08ac2f64a30e825a619fda63ea83
Alex Hamerling <alex.hamerling@gmail.com>
2016-03-20 17:22:58 +0100

    Intermediate commit

--- Assets/SpriterDotNetMecanim/AnimatorAssist.cs
+++ Assets/SpriterDotNetMecanim/AnimatorAssist.cs
@@ -9,8 +9,10 @@ namespace SpriterDotNetMecanim {
 	/// </summary>
 	[RequireComponent (typeof(SpriterDotNetBehaviour), typeof (Animator))]
 	public class AnimatorAssist : MonoBehaviour {
+		private Animator MecanimAnimator;
 		private UnitySpriterAnimator SpriterAnimator;
 		private Dictionary<int, string> Animations = new Dictionary<int, string> ();
+		private Dictionary<int, AnimatorRelay> Relays = new Dictionary<int, AnimatorRelay> ();
 
 		/// <summary>
 		/// The time it takes in milliseconds to transition from one animation to another.
@@ -19,11 +21,10 @@ namespace SpriterDotNetMecanim {
 		public float GlobalTransitionDuration = 500f;
 
 		private IEnumerator Start () {
-			do {
-				SpriterAnimator = GetComponent<SpriterDotNetBehaviour> ().Animator;
-				if (SpriterAnimator != null) break;
-				yield return null;
-			} while (true);
+			MecanimAnimator = GetComponent<Animator> ();
+			var behaviour = GetComponent<SpriterDotNetBehaviour> ();
+			while (behaviour.Animator == null) yield return null;
+			SpriterAnimator = behaviour.Animator;
 			foreach (var animation in SpriterAnimator.GetAnimations ())
 				Animations.Add (Animator.StringToHash (animation), animation);
 		}
@@ -31,11 +32,33 @@ namespace SpriterDotNetMecanim {
 		/// <summary>
 		/// Not meant to be called manually.
 		/// </summary>
-		public void Transition (int AnimationID, int LayerID) {
+		public void Transition (AnimatorRelay relay, int animationID, int layerID) {
+//			if (Relays.ContainsKey (Animator.StringToHash ("hit_0"))) {
+//				Debug.Log (Relays [Animator.StringToHash ("hit_0")].CustomTransitions [Animator.StringToHash ("idle")]);
+//			}
+			if (!Relays.ContainsKey (animationID)) {
+				Relays.Add (animationID, relay);
+			}
+//			foreach (var rrelay in Relays.Values) {
+//				Debug.Log (rrelay == relay);
+//			}
 			string animation;
 			var length = GlobalTransitionDuration;
-			if (Animations.TryGetValue (AnimationID, out animation))
+			if (MecanimAnimator.IsInTransition (layerID)) {
+				if (MecanimAnimator.GetAnimatorTransitionInfo (layerID).anyState) {
+				}
+				else {
+					var newLength = Relays [MecanimAnimator.GetCurrentAnimatorStateInfo (layerID).shortNameHash].CustomTransitions [animationID];
+					if (newLength >= 0) length = newLength;
+				}
+			}
+			if (Animations.TryGetValue (animationID, out animation))
 				SpriterAnimator.Transition (animation, length);
+			//Debug.Log (length);
+		}
+
+		public void TestThis (AnimatorRelay hitrelay) {
+//			Debug.Log (hitrelay == Relays [Animator.StringToHash ("hit_0")]);
 		}
 	}
 }
---
+++ Assets/SpriterDotNetMecanim/Editor/CustomEditors.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using System;
+using System.Collections.Generic;
+
+namespace SpriterDotNetMecanim.Editor {
+	using Editor = UnityEditor.Editor;
+	[CustomEditor (typeof (AnimatorRelay))] public class RelayEditor : Editor {
+		private new AnimatorRelay target;
+		private AnimatorState state;
+
+		private void OnEnable () {
+			target = (AnimatorRelay)base.target;
+			var context = AnimatorController.FindStateMachineBehaviourContext (target) [0];
+			state = context.animatorObject as AnimatorState;
+			if (!state) Debug.LogError ("AnimatorRelay will not work correctly if not attached to an AnimatorState");
+			if (target.CustomTransitions == null) target.CustomTransitions = new AnimatorRelay.TransitionList ();
+			var toRemove = new List<int> ();
+			foreach (var transition in target.CustomTransitions) {
+				var trans = ArrayUtility.Find (state.transitions, x => Match (x, transition.TargetID));
+				if (!trans) toRemove.Add (transition.TargetID);
+			}
+			foreach (var id in toRemove) target.CustomTransitions [id] = -1f;
+			target.testRelay = target;
+		}
+
+		private static bool Match (AnimatorStateTransition transition, int stateID) {
+			var state = transition.destinationState;
+			if (state) return state.nameHash == stateID;
+			else return false;
+		}
+
+		public override void OnInspectorGUI () {
+			DrawDefaultInspector ();
+			if (state.transitions.Length <= 0) return;
+			if (target.CustomTransitions.Count <= 0)
+				EditorGUILayout.LabelField ("No custom transitions have been set.");
+			else EditorGUILayout.LabelField ("Custom transition durations (in milliseconds):");
+			foreach (var transition in target.CustomTransitions) {
+				EditorGUILayout.BeginHorizontal ();
+				EditorGUI.BeginChangeCheck ();
+				var newID = Transitions (transition.TargetID);
+				if (EditorGUI.EndChangeCheck () && newID != transition.TargetID) {
+					target.CustomTransitions [transition.TargetID] = -1f;
+					target.CustomTransitions [newID] = transition.Duration;
+					break;
+				}
+				EditorGUI.BeginChangeCheck ();
+				var newDuration = EditorGUILayout.FloatField (transition.Duration);
+				if (EditorGUI.EndChangeCheck () && newDuration != transition.Duration)
+					target.CustomTransitions [transition.TargetID] = newDuration >= 0f ? newDuration : 0f;
+				if (GUILayout.Button ("x")) {
+					target.CustomTransitions [transition.TargetID] = -1f;
+					break;
+				}
+				EditorGUILayout.EndHorizontal ();
+			}
+			if (target.CustomTransitions.Count < state.transitions.Length && GUILayout.Button ("New custom transition")) {
+				foreach (var transition in state.transitions) {
+					var newState = transition.destinationState;
+					if (newState && !target.CustomTransitions.Contains (newState.nameHash)) {
+						target.CustomTransitions [newState.nameHash] = 500f;
+						break;
+					}
+				}
+			}
+			if (GUI.changed) EditorUtility.SetDirty (target);
+		}
+
+		private int Transitions (int currentVal) {
+			var transitions = new List<string> ();
+			var ids = new List<int> ();
+			foreach (var transition in state.transitions) {
+				var newState = transition.destinationState;
+				if (newState) {
+					var id = newState.nameHash;
+					if (currentVal != id && target.CustomTransitions.Contains (id)) continue;
+					transitions.Add (newState.name);
+					ids.Add (id);
+				}
+			}
+			return EditorGUILayout.IntPopup (currentVal, transitions.ToArray (), ids.ToArray ());
+		}
+	}
+}
